6678cf55bc792ca8f2ff2badbcafb095355d029a
Steven <otienosteven1233@gmail.com>
2026-05-16 01:13:15 +0300

    updated dashboard upload logic

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -338,6 +338,19 @@ const Dashboard = () => {
       {/* MAIN */}
       <main className="flex-1 md:ml-72 p-6">
 
+        <div className="md:hidden flex items-center justify-between mb-4 bg-white p-3 rounded-xl shadow-sm">
+  <h2 className="font-bold text-gray-800">
+    DRYME
+  </h2>
+
+  <button
+    onClick={handleLogout}
+    className="bg-red-600 text-white px-3 py-2 rounded-lg text-sm"
+  >
+    Logout
+  </button>
+</div>
+
         {/* HEADER */}
         <div className="mb-8">
 
