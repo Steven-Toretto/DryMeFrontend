98d274ae679abf780fce0035e58c28bc56a3f8c7
Steven <otienosteven1233@gmail.com>
2026-06-11 04:06:01 +0300

    removed commented out old codes

--- src/components/PrivateRouter.jsx
+++ src/components/PrivateRouter.jsx
@@ -15,20 +15,3 @@ function PrivateRoute({ children }) {
 export default PrivateRoute;
 
 
-// function PrivateRoute({ children, roleRequired }) {
-
-//   const token = localStorage.getItem("access");
-//   const user = JSON.parse(localStorage.getItem("user"));
-
-//   if (!token){
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   if (roleRequired && user?.role !== roleRequired) {
-//     return <Navigate to="/login" replace />;
-//   }
-
-//   return children;
-// }
-// export default PrivateRoute;
-
